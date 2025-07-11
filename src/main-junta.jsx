780daf394a63072d01ff30c46e0cd04e3bbcb100
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-07-11 19:27:44 -0300

    faltan estrilos

--- src/main-junta.jsx
+++ src/main-junta.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import "..src/index.css"; // O la ruta correcta según la estructura
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

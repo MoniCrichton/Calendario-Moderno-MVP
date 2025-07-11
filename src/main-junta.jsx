08837b654d8600c119dea34896cce7a93a555132
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-07-11 20:01:00 -0300

    error de signos

--- src/main-junta.jsx
+++ src/main-junta.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import "./index.css"; "// O la ruta correcta según la estructura
+import "./index.css"; // O la ruta correcta según la estructur
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(

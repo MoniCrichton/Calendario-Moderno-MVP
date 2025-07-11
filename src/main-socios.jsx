d30ba04c272031018640e0716bf7f5aebdc6bfd8
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-07-11 19:59:33 -0300

    fix3: nueva vorreccion de rutas

--- src/main-socios.jsx
+++ src/main-socios.jsx
@@ -1,7 +1,7 @@
   import React from 'react';
   import ReactDOM from 'react-dom/client';
   import App from './App.jsx';
-  import "..src/index.css"; // O la ruta correcta según la estructura
+  import "./index.css"; // O la ruta correcta según la estructura
 
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>

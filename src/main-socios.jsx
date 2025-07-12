eab05766c4ae80c38c2b9a2e8c54e24dd83ba0e1
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-07-12 17:08:04 -0300

    correccion en *.html, se agregó leyenda  <meta name="viewport" content="width=device-width, initial-scale=1" /> para conseguir responsive

--- src/main-socios.jsx
+++ src/main-socios.jsx
@@ -1,10 +1,13 @@
-  import React from 'react';
-  import ReactDOM from 'react-dom/client';
-  import App from './App.jsx';
-  import "./index.css"; // O la ruta correcta según la estructura
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import "./index.css";
+import { registerSW } from "virtual:pwa-register";
 
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App nivel="socios" />
-    </React.StrictMode>
-  );
+registerSW();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App nivel="socios" />
+  </React.StrictMode>
+);

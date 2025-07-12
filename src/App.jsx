792127ca409c8904971978b6bfaeee36dde13459
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-07-12 20:18:19 -0300

    actualizado app.jsx para tesoreria

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Calendario from "./pages/Calendario";
 import NotFound from "./pages/NotFound";
 import PanelEventos from "./pages/PanelEventos"; // ✅ Asegurate que la ruta coincida
 import PanelTesoreria from "./pages/PanelTesoreria";
+import CalendarioTesoreria from "./pages/CalendarioTesoreria";
 
 
 export default function App() {
@@ -14,7 +15,7 @@ export default function App() {
         <Route path="/calendario" element={<Calendario nivel="publico" />} />
         <Route path="/calendario-socios" element={<Calendario nivel="socio" />} />
         <Route path="/calendario-junta" element={<Calendario nivel="junta" />} />
-        <Route path="/calendario-tesoreria" element={<Calendario nivel="tesoreria" />} />
+        <Route path="/calendario-tesoreria" element={<CalendarioTesoreria />} />
         <Route path="/admin" element={<PanelEventos />} /> {/* ✅ Esta es la línea nueva */}
         <Route path="/panel-tesoreria" element={<PanelTesoreria />} />
         <Route path="*" element={<NotFound />} />

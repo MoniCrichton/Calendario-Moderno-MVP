d2b15c674802ff8e971c3d4caaa65149d70da52f
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-07-16 21:04:33 -0300

    BrowserRouter completo

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // 👈 Este es el import clave
+import './index.css'
 
+// Agregá esto:
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )

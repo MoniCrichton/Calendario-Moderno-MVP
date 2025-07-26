b78b7eaa94e4f1c3b205ac794ea42401d1df103f
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-07-26 20:34:18 -0300

    corrige estado cuenta sociojsx de components

--- src/main-estado-cuenta.jsx
+++ src/main-estado-cuenta.jsx
@@ -1,7 +1,7 @@
 // src/main-estado-cuenta.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import EstadoCuentaSocio from './pages/EstadoCuentaSocio';
+import EstadoCuentaSocio from './components/EstadoCuenta/EstadoCuentaSocio';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
